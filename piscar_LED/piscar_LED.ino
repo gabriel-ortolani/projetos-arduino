12128a555d423f67499e20959673f27fc3cffc52
Gabriel Ortolani <gabrielortolanisenai@gmail.com>
2024-08-05 15:10:05 -0300

    Foram adicionados comentarios ao codigo

--- piscar_LED/piscar_LED.ino
+++ piscar_LED/piscar_LED.ino
@@ -1,13 +1,14 @@
+//programa que envia sinal alto para a saída 9, aguarda 1 segundo e envia sinal baixo para a saída 9
 void setup() {
   // put your setup code here, to run once:
-  pinMode(9,OUTPUT);
+  pinMode(9,OUTPUT);//definindo a porta 9 como saída
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    digitalWrite(9,HIGH);
-    delay(1000);
-    digitalWrite(9,LOW);
-    delay(1000);
+    digitalWrite(9,HIGH);//envia sinal alto
+    delay(1000);//aguarde 1seg
+    digitalWrite(9,LOW);//envia sinal baixo
+    delay(1000);//aguarde 1seg
 }
